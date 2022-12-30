c3ee3e0fc4dfd0633b446cf222bd12a0f72c4150
YitayalT <anex518@gmail.com>
2022-12-30 05:43:01 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import DropDown from "./components/DropDown";
+// import Search from "./components/Search";
+// import Accordion from "./components/Accordion";
 
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content: 'React is a front end javascript framework.'
+//   },
+//   {
+//     title: 'why use react?',
+//     content: 'React is a favorite library used by engineers.'
+//   },
+//   {
+//     title: 'how do you use react?',
+//     content: 'React is used by creating components.'
+//   }
+// ]
+
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'Red'
+  },
+   {
+    label: 'The Color Green',
+    value: 'Green'
+  },
+  {
+    label: 'A Shade Of Blue',
+    value: 'Blue'
+  }
+];
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+  return ( 
+
+    <div>
+      <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
+      {showDropdown ? <DropDown selected = {selected} onSelectedChange = {setSelected} options={options } />: null}
+      {/* <Accordion items={ items} /> */}
+      {/* <Search /> */}
     </div>
-  );
+  ); 
 }
 
 export default App;
---
+++ src/components/Translate.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DropDown from './DropDown'
+
+function Translate() {
+  return (
+      <div>
+      <DropDown />
+      </div>
+  )
+}
+
+export default Translate
